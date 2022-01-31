0e18a3b329dfce4d1fa1bc581ca834f86cab8401
Niravlakhani <Niravlakhani020@gmail.com>
2022-01-31 21:10:57 +0530

    Array Program Complited

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("Hello");
+
+let a = [0,1,0,0,1,1,0,1,1,1,0,0,1];
+let max = 0;
+let maxindex = 0;
+
+for(let i = 0; i < a.length; i++){
+
+    if(a[i] == 0){
+        let count = 0;
+        for(let j = i + 1; j < a.length && a[j] == 1; j++){
+            count++;
+        }
+        for(let j = i -1; j > 0 && a[j] == 1; j--){
+            count++;
+        }
+        count++;
+        if(count > max){
+            max = count;
+            maxindex = i;
+        }
+        console.log("i" , i);
+        console.log("Count" , count);
+        a[i] = 0;
+    }
+
+}
+
+console.log("Max" , max);
+console.log("MaxIndex" , maxindex);
